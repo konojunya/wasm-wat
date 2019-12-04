26c9ccf61c293ef149a8c87e9aa65b9c2022b966
konojunya <junya03dance@gmail.com>
2019-12-05 01:19:46 +0900

    feat: add plusone.wat

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 async function fetchAndInstantiate() {
-  const res = await fetch("add.wasm");
+  const res = await fetch("plusone.wasm");
   const buffer = await res.arrayBuffer();
   const obj = await WebAssembly.instantiate(buffer)
   return obj;
 }
 
 fetchAndInstantiate().then(obj => {
-  console.log(obj.instance.exports.add(1,2))
+  // console.log(obj.instance.exports.add(1,2))
+  console.log(obj.instance.exports.getAnswerPlus1())
 })
