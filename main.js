01d1d222b985adffa0b6e849ccf18967a14c9835
konojunya <junya03dance@gmail.com>
2019-12-05 01:47:33 +0900

    feat: add logger2.wasm

--- main.js
+++ main.js
@@ -1,11 +1,67 @@
+// add.wasm
+// async function fetchAndInstantiate() {
+//   const res = await fetch("add.wasm");
+//   const buffer = await res.arrayBuffer();
+//   const obj = await WebAssembly.instantiate(buffer)
+//   return obj;
+// }
+//
+// fetchAndInstantiate().then(obj => {
+//   console.log(obj.instance.exports.add(1,2))
+// })
+
+// plusone.wasm
+// async function fetchAndInstantiate() {
+//   const res = await fetch("plusone.wasm");
+//   const buffer = await res.arrayBuffer();
+//   const obj = await WebAssembly.instantiate(buffer)
+//   return obj;
+// }
+//
+// fetchAndInstantiate().then(obj => {
+//   console.log(obj.instance.exports.getAnswerPlus1())
+// })
+
+// logger.wasm
+// async function fetchAndInstantiate() {
+//   const res = await fetch("logger.wasm");
+//   const buffer = await res.arrayBuffer();
+//   const obj = await WebAssembly.instantiate(buffer, {
+//     console: {
+//       log: function(arg) {
+//         console.log(arg)
+//       }
+//     }
+//   })
+//   return obj;
+// }
+//
+// fetchAndInstantiate().then(obj => {
+//   obj.instance.exports.logIt()
+// })
+
+// logger2.wasm
+const memory = new WebAssembly.Memory({initial: 1});
+function consoleLogString(offset, length) {
+  const bytes = new Uint8Array(memory.buffer, offset, length);
+  const string = new TextDecoder("utf8").decode(bytes);
+  console.log(string)
+}
 async function fetchAndInstantiate() {
-  const res = await fetch("plusone.wasm");
+  const res = await fetch("logger2.wasm");
   const buffer = await res.arrayBuffer();
-  const obj = await WebAssembly.instantiate(buffer)
+  const obj = await WebAssembly.instantiate(buffer, {
+    console: {
+      log: consoleLogString
+    },
+    js: {
+      mem: memory
+    }
+  })
   return obj;
 }
 
 fetchAndInstantiate().then(obj => {
-  // console.log(obj.instance.exports.add(1,2))
-  console.log(obj.instance.exports.getAnswerPlus1())
+  obj.instance.exports.writeHi()
 })
+
